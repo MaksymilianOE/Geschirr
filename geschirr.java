71d4156bac493d93fc88538cb8b587aa06ceb745
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2024-02-27 20:19:06 +0100

    2

--- geschirr.java
+++ geschirr.java
@@ -1,15 +1,26 @@
 import java.util.Arrays;
 import java.util.Objects;
 
+//TODO: Array auffüllen DONE
+//TODO: Array umdrehen
+//TODO: in Array reinböllern
+
 public class geschirr {
     public static void main(String[] args) {
-        //TODO: Array auffüllen DONE
-        //TODO: Array umdrehen
-        //TODO: 
+        String[] startArr;
+        startArr = args;
+        System.out.println("Startarr: " + Arrays.toString(startArr));
 
+
+        if (args.length > 5) {
+            System.out.println("Bitte nicht mehr als 5 Worter eingeben!");
+        }
+        if (args.length < 5) {
+            System.out.println("Bitte mindestens 5 Wörter eingeben! Es werden jetzt dummywerte verwendet!");
+        }
+        System.out.println(Arrays.toString(args));
         String[] dummyWerte = {"Teller", "Tasse", "Messer", "Gabel", "Pfanne"};
-        String[] testArr = {"Gabel", "Schüssel", "Kelle", "", "Topf"};
-        String[] returnArray = checkArray(testArr, dummyWerte);
+        String[] returnArray = checkArray(args, dummyWerte);
         System.out.println(Arrays.toString(returnArray));
     }
 
@@ -21,12 +32,25 @@ public class geschirr {
     }
 
     public static String[] checkArray (String[] geschirr, String[] dummy) {
-        for (int i = 0; i <= geschirr.length - 1; i++) {
-            if (Objects.equals(geschirr[i], "")) {
-                System.out.println("Eine Position ist leer! Verwende Dummywerte!");
-                geschirr[i] = dummy[i];
+        String[] returnArr = new String[5];
+        if (geschirr.length == 5) {
+            //CODE
+            for (int i = 0; i <= geschirr.length - 1; i++) {
+                System.out.println(geschirr[i]);
+                if (Objects.equals(geschirr[i], "") || geschirr[i] == null) {
+                    System.out.println("Dummywerte werden für fehlenden Eintrag verwendet!");
+                    returnArr[i] = dummy[i];
+                } else {
+                    returnArr[i] = geschirr[i];
+                }
+            }
+        } else {
+            if (geschirr.length > 5) {
+                System.out.println("Es werden nur die ersten 5 Wörter berücksichtigt!");
+            } else {
+                System.out.println("Zu wenige Werte (Min 5). Es werden Dummywerte verwendet!");
             }
         }
-        return geschirr;
+        return returnArr;
     }
 }
