fe8dcf99922b329fb3c368517fec358de8185fbe
Pawiancoder <110484323+Pawiancoder@users.noreply.github.com>
2024-02-27 20:52:19 +0100

    Update geschirr.java

--- geschirr.java
+++ geschirr.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Objects;
 
 //TODO: Array auffüllen DONE
 //TODO: Array umdrehen
@@ -7,50 +6,51 @@ import java.util.Objects;
 
 public class geschirr {
     public static void main(String[] args) {
-        String[] startArr;
-        startArr = args;
-        System.out.println("Startarr: " + Arrays.toString(startArr));
+        try {
+            String[] startArr;
+            startArr = args;
+            System.out.println("Startarr: " + Arrays.toString(startArr));
 
-
-        if (args.length > 5) {
-            System.out.println("Bitte nicht mehr als 5 Worter eingeben!");
+            String[] dummyWerte = {"Teller", "Tasse", "Messer", "Gabel", "Pfanne"};
+            String[] checkedArray = checkArray(args, dummyWerte);
+            //System.out.println(Arrays.toString(checkedArray));
+            System.out.println(Arrays.toString(turnArray(checkedArray)));
+        } catch (Exception e) {
+            System.out.println("Ein Fehler ist aufgetreten! Bitte versche es erneut! " + e);
         }
-        if (args.length < 5) {
-            System.out.println("Bitte mindestens 5 Wörter eingeben! Es werden jetzt dummywerte verwendet!");
         }
-        System.out.println(Arrays.toString(args));
-        String[] dummyWerte = {"Teller", "Tasse", "Messer", "Gabel", "Pfanne"};
-        String[] returnArray = checkArray(args, dummyWerte);
-        System.out.println(Arrays.toString(returnArray));
-    }
 
-    public static String[] turnArray (String[] arr, String[] dummyWerte) {
+    public static String[] turnArray(String[] arr) {
+        //System.out.println("Argumente: " + Arrays.toString(arr));
         String[] returnArray = new String[5]; //array zum befüllen
         int counter = 0;
-
+        for (int i = returnArray.length - 1; i >= 0; i--) {
+            returnArray[counter] = arr[i];
+            counter++;
+        }
         return returnArray;
     }
 
-    public static String[] checkArray (String[] geschirr, String[] dummy) {
-        String[] returnArr = new String[5];
-        if (geschirr.length == 5) {
-            //CODE
-            for (int i = 0; i <= geschirr.length - 1; i++) {
-                System.out.println(geschirr[i]);
-                if (Objects.equals(geschirr[i], "") || geschirr[i] == null) {
-                    System.out.println("Dummywerte werden für fehlenden Eintrag verwendet!");
-                    returnArr[i] = dummy[i];
-                } else {
-                    returnArr[i] = geschirr[i];
+    public static String[] checkArray(String[] args, String[] dummy) {
+
+        String[] returnArray = new String[5];
+        if (args.length == 5) {
+            System.arraycopy(args, 0, returnArray, 0, args.length - 1 + 1); //Kopiert den Array (Vorschlag IDE) ?!
+        } else if (args.length > 5) {
+            System.out.println("Es wurden zu viele Untensilien angegeben! Es werden nur die ersten 5 berücksichtigt!");
+            System.arraycopy(args, 0, returnArray, 0, args.length - 1); //Kopiert den Array (Vorschlag IDE) ?!
+        } else {
+            {
+                System.out.println("DU KEK hast zu wenige Untensilien angegeben! Die Fehlenden Plätze werden mit Dummy-Werten befüllt!");
+                for (int i = 0; i < returnArray.length; i++) {
+                    if (i < args.length && args[i] != null) {
+                        returnArray[i] = args[i];
+                    } else {
+                        returnArray[i] = dummy[i];
+                    }
                 }
             }
-        } else {
-            if (geschirr.length > 5) {
-                System.out.println("Es werden nur die ersten 5 Wörter berücksichtigt!");
-            } else {
-                System.out.println("Zu wenige Werte (Min 5). Es werden Dummywerte verwendet!");
-            }
         }
-        return returnArr;
+        return returnArray;
     }
 }
